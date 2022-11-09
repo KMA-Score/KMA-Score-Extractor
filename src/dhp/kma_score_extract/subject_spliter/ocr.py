83e38d9cab5e834bbbff3c33bc3199c3ceb25786
Dang Hoang Phuc <dhp371.work@gmail.com>
2022-11-09 12:18:05 +0700

    BREAKING CHANGE: REMOVE  ALL V1 FILE

--- src/dhp/kma_score_extract/subject_spliter/ocr.py
+++
@@ -1,9 +0,0 @@
-import easyocr
-
-
-def ocr(img, gpu=True):
-    reader = easyocr.Reader(['vi', 'en'], gpu=gpu)
-    text = reader.readtext(img, detail=0)
-    text = " ".join(text)
-
-    return text
